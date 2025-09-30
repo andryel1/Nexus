e266cfd65c8025d30496b5f816851000486a7518
Andryel <andryelbonfim01@gmail.com>
2025-09-29 21:28:38 -0300

    Atualiza models.py do app reservations

--- reservations_service/reservations/models.py
+++ reservations_service/reservations/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Reservation(models.Model):
+    date_start = models.DateField()
+    date_end = models.DateField()
+    
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('confirmed', 'Confirmed'),
+        ('canceled', 'Canceled'),
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+
+    def __str__(self):
+        return f"Reservation from {self.date_start} to {self.date_end} - {self.status}"
